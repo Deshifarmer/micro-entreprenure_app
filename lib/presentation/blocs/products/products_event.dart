94a41f7db57794dec3238b3ba392667cac647d68
Alif Sakib <alifsakib@gmail.com>
2023-07-27 16:04:23 +0600

    product fetch event paginate

--- lib/presentation/blocs/products/products_event.dart
+++ lib/presentation/blocs/products/products_event.dart
@@ -11,3 +11,12 @@ class ProductFetchEvent extends ProductsEEvent {
   const ProductFetchEvent(this.token);
   final String token;
 }
+
+class ProductFetchPaginationEvent extends ProductsEEvent {
+  const ProductFetchPaginationEvent({
+    required this.nextPage,
+    required this.token,
+  });
+  final String token;
+  final int nextPage;
+}
