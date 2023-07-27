20dba2d502492a499c51c35130c73f42a1b1719e
RomjanHossain <romjan@skiff.com>
2023-07-27 18:04:29 +0600

    company select grid view

--- lib/presentation/pages/products/widgets/product_gird_view.dart
+++ lib/presentation/pages/products/widgets/product_gird_view.dart
@@ -1,34 +1,36 @@
 import 'package:deshifarmer/core/app_strings.dart';
 import 'package:deshifarmer/presentation/blocs/products/products_bloc.dart';
 import 'package:deshifarmer/presentation/pages/commision/bloc/bloc.dart';
+import 'package:deshifarmer/presentation/pages/products/bloc/products_bloc.dart';
 import 'package:flutter/material.dart';
 
 class ProductGridViewWidgets extends StatelessWidget {
   const ProductGridViewWidgets({
-    // required this.token,
     super.key,
   });
-  // final ScrollController _scrollController = ScrollController();
-  // final List<ProductData> _products = <ProductData>[];
-  // final int lastPage = 1;
-  // final String token;
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<ProductsBBloc, ProductsSState>(
       listener: (context, ProductsSState state) {},
       builder: (context, state) {
         if (state is ProductSuccess) {
+          final companyState = context.read<ProductsBloc>().state;
+          final allProducts = companyState is ProductComanySelect
+              ? state.productDatas
+                  .where(
+                      (element) => element.company_id == companyState.companyID)
+                  .toList()
+              : state.productDatas;
           return GridView.builder(
             shrinkWrap: true,
-            itemCount: state.productDatas.length,
+            itemCount: allProducts.length,
             physics: const NeverScrollableScrollPhysics(),
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               mainAxisExtent: 300,
             ),
             itemBuilder: (context, index) {
-              final product = state.productDatas.elementAt(index);
-              // print('${Strings.domain}/storage${product?.image}');
+              final product = allProducts.elementAt(index);
               return Padding(
                 padding: const EdgeInsets.all(8),
                 child: Card(
@@ -38,6 +40,7 @@ class ProductGridViewWidgets extends StatelessWidget {
                         margin: const EdgeInsets.all(10),
                         height: 150,
                         // color: Colors.greenAccent,
+                        // ignore: unnecessary_null_comparison
                         child: product.image != null
                             ? ClipRRect(
                                 borderRadius: BorderRadius.circular(10),
@@ -71,6 +74,8 @@ class ProductGridViewWidgets extends StatelessWidget {
             },
           );
         }
+        // return
+
         if (state is ProductFailed) {
           return const Center(
             child: Text('Failed'),
