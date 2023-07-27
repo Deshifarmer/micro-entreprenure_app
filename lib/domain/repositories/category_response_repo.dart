19bc88a0b95e6e7f154e9be3d0b42f6f672c1a1f
RomjanHossain <romjan@skiff.com>
2023-07-27 16:00:46 +0600

    unnecessary import again

--- lib/domain/repositories/category_response_repo.dart
+++ lib/domain/repositories/category_response_repo.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: one_member_abstracts
 
 import 'package:deshifarmer/core/error/exceptions.dart';
-import 'package:deshifarmer/domain/entities/company_entity/all_company_entity.dart';
-
-import '../entities/category_entity/all_categorys.dart';
+import 'package:deshifarmer/domain/entities/category_entity/all_categorys.dart';
 
 /// ShortUrlRepository
 abstract class CategoryListRepo {
