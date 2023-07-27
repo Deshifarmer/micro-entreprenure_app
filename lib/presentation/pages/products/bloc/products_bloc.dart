73ac56f76c7035155fb87586a9a7da50c27917de
Alif Sakib <alifsakib@gmail.com>
2023-07-27 18:02:34 +0600

    select and unselect company states

--- lib/presentation/pages/products/bloc/products_bloc.dart
+++ lib/presentation/pages/products/bloc/products_bloc.dart
@@ -8,12 +8,28 @@ part 'products_state.dart';
 class ProductsBloc extends Bloc<ProductsEvent, ProductsState> {
   ProductsBloc() : super(const ProductsInitial()) {
     on<CustomProductsEvent>(_onCustomProductsEvent);
+    on<SelectCompanysEvent>(_selectCompanyEvent);
+    on<UnSelectCompanyEvent>(_unSelectCompanyEvent);
   }
 
   FutureOr<void> _onCustomProductsEvent(
     CustomProductsEvent event,
     Emitter<ProductsState> emit,
+  ) {}
+
+  ///? Select company
+  FutureOr<void> _selectCompanyEvent(
+    SelectCompanysEvent event,
+    Emitter<ProductsState> emit,
   ) {
-    // TODO: Add Logic
+    emit(ProductComanySelect(event.companyID));
+  }
+
+  ///? unselect company
+  FutureOr<void> _unSelectCompanyEvent(
+    UnSelectCompanyEvent event,
+    Emitter<ProductsState> emit,
+  ) {
+    emit(const ProductsInitial());
   }
 }
