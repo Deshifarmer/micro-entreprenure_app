421522d5fe3099811ccbe8c7b2069e5c0689ce09
RomjanHossain <romjan@skiff.com>
2023-07-27 18:01:52 +0600

    select and unselect event

--- lib/presentation/pages/products/bloc/products_event.dart
+++ lib/presentation/pages/products/bloc/products_event.dart
@@ -1,17 +1,24 @@
 part of 'products_bloc.dart';
 
-abstract class ProductsEvent  extends Equatable {
+abstract class ProductsEvent extends Equatable {
   const ProductsEvent();
 
   @override
   List<Object> get props => [];
-
 }
 
 /// {@template custom_products_event}
 /// Event added when some custom logic happens
 /// {@endtemplate}
 class CustomProductsEvent extends ProductsEvent {
-  /// {@macro custom_products_event}
   const CustomProductsEvent();
+}
+
+class SelectCompanysEvent extends ProductsEvent {
+  const SelectCompanysEvent(this.companyID);
+  final String companyID;
+}
+
+class UnSelectCompanyEvent extends ProductsEvent {
+  const UnSelectCompanyEvent();
 }
