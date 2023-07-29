d272f412a82389a287caf92bbf25024f50eec7cf
Alif Sakib <alifsakib@gmail.com>
2023-07-29 18:01:04 +0600

    cart event

---
+++ lib/presentation/blocs/cart/cart_event.dart
@@ -0,0 +1,27 @@
+part of 'cart_bloc.dart';
+
+abstract class CartEvent extends Equatable {
+  const CartEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AddtoCartEvent extends CartEvent {
+  const AddtoCartEvent({
+    required this.productData,
+    required this.quantity,
+  });
+  final int quantity;
+  final ProductData productData;
+}
+
+// class RemoveFromCart extends Equatable{}
+class RemoveFromCart extends CartEvent {
+  const RemoveFromCart({
+    required this.productData,
+    required this.quantity,
+  });
+  final int quantity;
+  final ProductData productData;
+}
