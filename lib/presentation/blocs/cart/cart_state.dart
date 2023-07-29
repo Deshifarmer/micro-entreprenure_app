4e4742490f1899150c4482aa9bbc01691ba43486
Alif Sakib <alifsakib@gmail.com>
2023-07-29 18:01:11 +0600

    cart states

---
+++ lib/presentation/blocs/cart/cart_state.dart
@@ -0,0 +1,17 @@
+part of 'cart_bloc.dart';
+
+abstract class CartState extends Equatable {
+  const CartState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CartInitial extends CartState {}
+
+class CartAddingState extends CartState {
+  const CartAddingState(this.carts);
+  final Map<String, CartModel> carts;
+  @override
+  List<Object> get props => [carts];
+}
