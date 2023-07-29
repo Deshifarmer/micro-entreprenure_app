9196a58c4afbdb54d8c0889e07f0927d788fae78
RomjanHossain <romjan@skiff.com>
2023-07-29 18:05:20 +0600

    invinsiable

---
+++ lib/presentation/pages/products/components/product_image.dart
@@ -0,0 +1,31 @@
+import 'package:deshifarmer/core/app_strings.dart';
+import 'package:deshifarmer/domain/entities/products_entity/product_data_entity.dart';
+import 'package:flutter/material.dart';
+
+class ProductImage extends StatelessWidget {
+  const ProductImage({
+    required this.product, super.key,
+  });
+
+  final ProductData product;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Image.network(
+        '${Strings.domain}/storage${product.image}',
+        errorBuilder: (context, error, stackTrace) {
+          return Center(
+              child: Text(
+            'Image Error',
+            style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Colors.redAccent,
+                  fontStyle: FontStyle.italic,
+                ),
+          ),);
+        },
+      ),
+    );
+  }
+}
