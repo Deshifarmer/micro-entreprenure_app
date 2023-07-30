7d3031b09296a3e54239c7450f3ccdf083da8f4e
RomjanHossain <romjan@skiff.com>
2023-07-30 14:39:33 +0600

    cart bloc reconfigured

--- lib/presentation/blocs/cart/cart_bloc.dart
+++ lib/presentation/blocs/cart/cart_bloc.dart
@@ -8,37 +8,67 @@ import 'package:equatable/equatable.dart';
 part 'cart_event.dart';
 part 'cart_state.dart';
 
+final Map<String, (ProductData, int)> _carts = <String, (ProductData, int)>{};
+
 class CartBloc extends Bloc<CartEvent, CartState> {
   CartBloc() : super(CartInitial()) {
     on<CartEvent>((event, emit) {});
     on<AddtoCartEvent>(_addtoCartEvent);
+    on<RemoveFromCart>(_removeFromCart);
   }
-  final Map<String, CartModel> _carts = <String, CartModel>{};
   FutureOr<void> _addtoCartEvent(AddtoCartEvent event, emit) async {
+    emit(CartInitial());
     //! check if the key exists
     if (_carts.containsKey(event.productData.product_id)) {
-      //!---------------DO NOT REMOVE--------------------!!!
-      if (event.quantity == 0) {
-        _carts.remove(event.productData.product_id);
-      }
-      //!------- then update it's value (only quantity)
+      var currentQ = _carts[event.productData.product_id]!.$2;
+      print(
+          'key already exists -> $currentQ : ${_carts[event.productData.product_id]?.$1.name}');
       _carts.update(
         event.productData.product_id ?? '',
-        (value) => CartModel(
-          productData: event.productData,
-          quantity: event.quantity,
-        ),
+        (value) => (event.productData, ++currentQ),
       );
-    } else
-    //! if not then add the key+value
-    {
-      _carts[event.productData.product_id ?? ''] = CartModel(
-        productData: event.productData,
-        quantity: event.quantity,
-      );
-    }
 
-    ///* emitting the states
-    emit(CartAddingState(_carts));
+      ///* emitting the states
+      await emit(CartAddingState(_carts));
+      print('q updated -> $currentQ');
+    } else {
+      print('${event.productData.name} not exists. adding.............');
+      _carts[event.productData.product_id ?? ''] = (event.productData, 1);
+      await emit(CartAddingState(_carts));
+    }
+  }
+
+  FutureOr<void> _removeFromCart(RemoveFromCart event, emit) async {
+    //! check if the key exists
+    if (_carts.containsKey(event.productData.product_id)) {
+      var currentQ = _carts[event.productData.product_id]!.$2;
+      currentQ--;
+      //! if quantity is being 01 then remove the element
+      if (currentQ < 1) {
+        _carts.remove(event.productData.product_id);
+
+        ///* emitting the states
+        emit(CartAddingState(_carts));
+      } else {
+        //! if exists then remove it's quantity
+        // _carts.update(
+        //   event.productData.product_id ?? '',
+        //   (value) => CartModel(
+        //     productData: event.productData,
+        //     quantity: ++currentQ,
+        //   ),
+        // );
+        _carts.update(
+          event.productData.product_id ?? '',
+          (value) => (event.productData, --currentQ),
+        );
+
+        ///* emitting the states
+        emit(CartAddingState(_carts));
+      }
+
+      //! if not then DO NOTHING
+      ///! NO CODE MEANS THERE'S NOTHING (XD)
+    }
   }
 }
