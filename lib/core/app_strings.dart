67cb735fc0730333ea3c53be57e90bb2fb101d24
RomjanHossain <romjan@skiff.com>
2023-07-30 18:03:20 +0600

    TOP SECRET

--- lib/core/app_strings.dart
+++ lib/core/app_strings.dart
@@ -2,6 +2,19 @@ class Strings {
   Strings._();
 
   static const String title = 'Deshifarmer';
-  // static const String domain = 'http://192.168.68.115:8000';
+  // static const String domain = 'http://192.168.68.126:8000';
   static const String domain = 'https://core.deshifarmer.co';
 }
+
+
+///!---------------------TOP SECRET ---------------------------
+/*
+01896267197
+df62me
+*/
+
+///!--------------------PUBLIC Secret -------------------------
+/*
+nadia@gmail.com
+password
+*/
