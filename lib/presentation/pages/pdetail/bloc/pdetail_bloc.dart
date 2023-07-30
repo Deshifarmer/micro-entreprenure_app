4f9b8e76327066e00d31ef9938344867d4a06237
Alif Sakib <alifsakib@gmail.com>
2023-07-30 14:40:41 +0600

    product detail bloc

---
+++ lib/presentation/pages/pdetail/bloc/pdetail_bloc.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+part 'pdetail_event.dart';
+part 'pdetail_state.dart';
+
+class PdetailBloc extends Bloc<PdetailEvent, PdetailState> {
+  PdetailBloc() : super(const PdetailInitial()) {
+    on<CustomPdetailEvent>(_onCustomPdetailEvent);
+  }
+
+  FutureOr<void> _onCustomPdetailEvent(
+    CustomPdetailEvent event,
+    Emitter<PdetailState> emit,
+  ) {
+    // TODO: Add Logic
+  }
+}
