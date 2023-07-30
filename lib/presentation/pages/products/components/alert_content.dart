88187f6e6638c03cc2a85708017f36e930483d4f
RomjanHossain <romjan@skiff.com>
2023-07-30 14:41:53 +0600

    alert content image hero

--- lib/presentation/pages/products/components/alert_content.dart
+++ lib/presentation/pages/products/components/alert_content.dart
@@ -5,45 +5,51 @@ import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 class AlertDialogContent extends StatelessWidget {
   const AlertDialogContent({
-    super.key,
-    required this.product,
+    required this.product, super.key,
   });
 
   final ProductData product;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        /// product image
-        if (product.image != null) ProductImage(product: product),
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        children: [
+          /// product image
+          if (product.image != null)
+            Hero(
+              tag: product.product_id ?? '',
+              child: ProductImage(product: product),
+            ),
 
-        /// product price
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: Text(
-            "${product.sell_price ?? ''} টাকা",
-            style: Theme.of(context).textTheme.titleLarge,
+          /// product price
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              "${product.sell_price ?? ''} টাকা",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
           ),
-        ),
-        Text(
-          product.company ?? '',
-          style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                fontStyle: FontStyle.italic,
-              ),
-        ),
+          Text(
+            product.company ?? '',
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  fontStyle: FontStyle.italic,
+                ),
+          ),
 
-        /// product category
-        Text(
-          product.category ?? '',
-          style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                fontStyle: FontStyle.italic,
-              ),
-        ),
+          /// product category
+          Text(
+            product.category ?? '',
+            style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                  fontStyle: FontStyle.italic,
+                ),
+          ),
 
-        /// product description
-        HtmlWidget(product.description ?? ''),
-      ],
+          /// product description
+          HtmlWidget(product.description ?? ''),
+        ],
+      ),
     );
   }
 }
