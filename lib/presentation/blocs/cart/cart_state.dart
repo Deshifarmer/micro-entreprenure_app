d8d17081ca3b688c2f217a10a0bfcf7564b3204e
Alif Sakib <alifsakib@gmail.com>
2023-07-31 14:10:57 +0600

    get total price from states

--- lib/presentation/blocs/cart/cart_state.dart
+++ lib/presentation/blocs/cart/cart_state.dart
@@ -13,6 +13,16 @@ class CartAddingState extends CartState {
   const CartAddingState(this.carts);
   final Map<String, (ProductData, int)> carts;
 
+  double getTotalPrices() {
+    var total = 0.0;
+    for (final element in carts.values) {
+      total =
+          total + (element.$2 * double.parse(element.$1.sell_price ?? '0'));
+    }
+
+    return total;
+  }
+
   @override
   List<Object> get props => [carts];
 }
