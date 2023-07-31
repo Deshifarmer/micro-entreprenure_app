ebaa1bab633c657c4425d0595163b5d03a9f2e0f
YamazakiKunq <yamazakik447@gmail.com>
2023-07-31 17:56:03 +0600

    isar db commented

--- lib/data/datasources/local/isar/isar_db_impl.dart
+++ lib/data/datasources/local/isar/isar_db_impl.dart
@@ -1,43 +1,43 @@
-import 'package:deshifarmer/data/datasources/models/user_profile_db.dart';
-import 'package:isar/isar.dart';
-import 'package:path_provider/path_provider.dart';
+// import 'package:deshifarmer/data/datasources/models/user_profile_db.dart';
+// import 'package:isar/isar.dart';
+// import 'package:path_provider/path_provider.dart';
 
-class IsarDatabase {
-  IsarDatabase() {
-    // ignore: discarded_futures
-    db = openIsar();
-  }
-  late Future<Isar> db;
+// class IsarDatabase {
+//   IsarDatabase() {
+//     // ignore: discarded_futures
+//     db = openIsar();
+//   }
+//   late Future<Isar> db;
 
-  ///! Get user profile ENTITY
-  // Future<UserProfileEntity> _getUserProfileEntity() async {
-  //   final isar = await db;
-  //   final userScheme = await isar.userProfileCollections.where().findAll();
-  //   final userProfile = userScheme.first;
-  // }
+//   ///! Get user profile ENTITY
+//   // Future<UserProfileEntity> _getUserProfileEntity() async {
+//   //   final isar = await db;
+//   //   final userScheme = await isar.userProfileCollections.where().findAll();
+//   //   final userProfile = userScheme.first;
+//   // }
 
-  ///! Insert User Profile
-  // Future<bool> insertUserProfile(UserProfileEntity upe) async{
-  //    final Isar isar = await db;
-  //   isar.writeTxnSync<int>(
-  //     () => isar.userProfileCollections.putSync(
-  //       upe,
-  //     ),
-  //   );
-  //   return true;
-  // }
+//   ///! Insert User Profile
+//   // Future<bool> insertUserProfile(UserProfileEntity upe) async{
+//   //    final Isar isar = await db;
+//   //   isar.writeTxnSync<int>(
+//   //     () => isar.userProfileCollections.putSync(
+//   //       upe,
+//   //     ),
+//   //   );
+//   //   return true;
+//   // }
 
-  /// open the db
-  Future<Isar> openIsar() async {
-    final dir = await getApplicationDocumentsDirectory();
-    if (Isar.instanceNames.isEmpty) {
-      return Isar.open(
-        <CollectionSchema<dynamic>>[
-          UserProfileCollectionSchema,
-        ],
-        directory: dir.path,
-      );
-    }
-    return Future<Isar>.value(Isar.getInstance());
-  }
-}
+//   /// open the db
+//   Future<Isar> openIsar() async {
+//     final dir = await getApplicationDocumentsDirectory();
+//     if (Isar.instanceNames.isEmpty) {
+//       return Isar.open(
+//         <CollectionSchema<dynamic>>[
+//           UserProfileCollectionSchema,
+//         ],
+//         directory: dir.path,
+//       );
+//     }
+//     return Future<Isar>.value(Isar.getInstance());
+//   }
+// }
