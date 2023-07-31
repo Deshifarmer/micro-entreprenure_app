700c3709c4de85a443aaa4650d5aa3d13d6d6a4f
RomjanHossain <romjan@skiff.com>
2023-07-31 14:08:08 +0600

    farmer repo implementations

---
+++ lib/data/repositories/farmer_repo_impl.dart
@@ -0,0 +1,13 @@
+import 'package:deshifarmer/core/error/exceptions.dart';
+import 'package:deshifarmer/data/datasources/remote/apis/api_source.dart';
+import 'package:deshifarmer/domain/entities/farmer_entity/all_farmer_entity.dart';
+import 'package:deshifarmer/domain/repositories/farmer_response_repo.dart';
+
+class FarmerListImpl extends FarmerListRepo {
+  final DeshiFarmerAPI _dfAPi = DeshiFarmerAPI();
+
+  @override
+  Future<Result<AllFarmerListResp, Exception>> getFarmers(String token) async {
+    return _dfAPi.getFarmers(token);
+  }
+}
