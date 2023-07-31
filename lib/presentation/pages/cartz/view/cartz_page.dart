7f30c1135a26f8c2e8f82b4f3b6a807daa7cd274
RomjanHossain <romjan@skiff.com>
2023-07-31 14:13:43 +0600

    order now button

--- lib/presentation/pages/cartz/view/cartz_page.dart
+++ lib/presentation/pages/cartz/view/cartz_page.dart
@@ -1,4 +1,3 @@
-import 'package:deshifarmer/presentation/pages/cartz/bloc/bloc.dart';
 import 'package:deshifarmer/presentation/pages/cartz/widgets/cartz_body.dart';
 import 'package:flutter/material.dart';
 
@@ -21,6 +20,19 @@ class CartzPage extends StatelessWidget {
         title: const Text('ব্যাগের পণ্য'),
       ),
       body: const CartzView(),
+      bottomNavigationBar: BottomAppBar(
+        child: ElevatedButton(
+          style: const ButtonStyle(
+            surfaceTintColor: MaterialStatePropertyAll(Colors.green),
+            shadowColor: MaterialStatePropertyAll(Colors.greenAccent),
+            // overlayColor: MaterialStatePropertyAll(
+            //   Colors.green[600],
+            // ),
+          ),
+          onPressed: () {},
+          child: const Text('অর্ডার করুন'),
+        ),
+      ),
     );
   }
 }
