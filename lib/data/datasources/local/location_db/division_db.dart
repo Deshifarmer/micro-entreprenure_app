78411cf441ac1b13794fce4094a56ca1856729da
RomjanHossain <romjan@skiff.com>
2023-08-01 17:53:25 +0600

    Dvision Database

---
+++ lib/data/datasources/local/location_db/division_db.dart
@@ -0,0 +1,53 @@
+import 'package:deshifarmer/data/models/location_model.dart';
+
+const List<DivisionModel> divisionDatabase = <DivisionModel>[
+  DivisionModel(
+      id: '1',
+      name: 'Barishal',
+      bnName: 'বরিশাল',
+      lat: '22.701002',
+      long: '90.353451',),
+  DivisionModel(
+      id: '2',
+      name: 'Chattogram',
+      bnName: 'চট্টগ্রাম',
+      lat: '22.356851',
+      long: '91.783182',),
+  DivisionModel(
+      id: '3',
+      name: 'Dhaka',
+      bnName: 'ঢাকা',
+      lat: '23.810332',
+      long: '90.412518',),
+  DivisionModel(
+      id: '4',
+      name: 'Khulna',
+      bnName: 'খুলনা',
+      lat: '22.845641',
+      long: '89.540328',),
+  DivisionModel(
+      id: '5',
+      name: 'Rajshahi',
+      bnName: 'রাজশাহী',
+      lat: '24.363589',
+      long: '88.624135',),
+  DivisionModel(
+      id: '6',
+      name: 'Rangpur',
+      bnName: 'রংপুর',
+      lat: '25.743892',
+      long: '89.275227',),
+  DivisionModel(
+    id: '7',
+    name: 'Sylhet',
+    bnName: 'সিলেট',
+    lat: '24.894929',
+    long: '91.868706',
+  ),
+  DivisionModel(
+      id: '8',
+      name: 'Mymensingh',
+      bnName: 'ময়মনসিংহ',
+      lat: '24.747149',
+      long: '90.420273',),
+];
