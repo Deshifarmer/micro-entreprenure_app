33fc6cc0cddbc701b2dd81ea707668a4473980bb
YamazakiKunq <yamazakik447@gmail.com>
2023-08-01 17:54:24 +0600

    all farmer group model

---
+++ lib/domain/entities/group_field_entity/all_farmer_group_field.dart
@@ -0,0 +1,7 @@
+
+import 'package:deshifarmer/domain/entities/group_field_entity/group_field_entity.dart';
+
+class AllFarmerGroupFieldResp {
+  AllFarmerGroupFieldResp (this.farmers);
+  final List<GroupFieldEntity> farmers;
+}
