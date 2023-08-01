9fbdc40d1b2e953b8205ce0ea30e2be013e23e44
RomjanHossain <romjan@skiff.com>
2023-08-01 10:30:31 +0600

    farmer profile pic component

---
+++ lib/presentation/pages/add_farmer/components/farmer_profile_field.dart
@@ -0,0 +1,109 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class FarmerProfilePicUpload extends StatefulWidget {
+  const FarmerProfilePicUpload({
+    super.key,
+  });
+
+  @override
+  State<FarmerProfilePicUpload> createState() => _FarmerProfilePicUploadState();
+}
+
+class _FarmerProfilePicUploadState extends State<FarmerProfilePicUpload> {
+  XFile? image;
+
+  final ImagePicker picker = ImagePicker();
+
+  //we can upload image from camera or from gallery based on parameter
+  Future getImage(ImageSource media) async {
+    final img = await picker.pickImage(source: media);
+
+    setState(() {
+      image = img;
+    });
+  }
+
+  //show popup dialog
+  void myAlert() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          title: const Text('Please choose media to select'),
+          content: SizedBox(
+            height: MediaQuery.of(context).size.height / 6,
+            child: Column(
+              children: [
+                ElevatedButton(
+                  //if user click this button, user can upload image from gallery
+                  onPressed: () {
+                    Navigator.pop(context);
+                    getImage(ImageSource.gallery);
+                  },
+                  child: const Row(
+                    children: [
+                      Icon(Icons.image),
+                      Text('From Gallery'),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  //if user click this button. user can upload image from camera
+                  onPressed: () {
+                    Navigator.pop(context);
+                    getImage(ImageSource.camera);
+                  },
+                  child: const Row(
+                    children: [
+                      Icon(Icons.camera),
+                      Text('From Camera'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: InkWell(
+        onTap: myAlert,
+        child: SizedBox(
+          height: 250,
+          width: 200,
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            alignment: Alignment.center,
+            // padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.greenAccent,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: image != null
+                ? ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.file(
+                      File(image!.path),
+                      fit: BoxFit.cover,
+                      height: 250,
+                    ),
+                  )
+                : const Icon(
+                    Icons.person,
+                    size: 200,
+                  ),
+          ),
+        ),
+      ),
+    );
+  }
+}
