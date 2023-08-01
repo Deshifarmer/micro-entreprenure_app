511cf54ff4454fe62ba97df534641886e2516414
YamazakiKunq <yamazakik447@gmail.com>
2023-08-01 17:52:56 +0600

    Corps Databases

---
+++ lib/data/datasources/local/corps/corps_db.dart
@@ -0,0 +1,180 @@
+import 'package:deshifarmer/data/datasources/local/corps/corps_model.dart';
+
+const List<CorpModel> cropsDatabase = <CorpModel>[
+  CorpModel(
+    id: 1,
+    name: 'ধান',
+  ),
+  CorpModel(
+    id: 2,
+    name: 'পাট',
+  ),
+  CorpModel(
+    id: 3,
+    name: 'গম',
+  ),
+  CorpModel(
+    id: 4,
+    name: 'আলু',
+  ),
+  CorpModel(
+    id: 5,
+    name: 'ভুট্টা',
+  ),
+  CorpModel(
+    id: 6,
+    name: 'আখের',
+  ),
+  CorpModel(
+    id: 7,
+    name: 'চা',
+  ),
+  CorpModel(
+    id: 8,
+    name: 'মসুর ডাল',
+  ),
+  CorpModel(
+    id: 9,
+    name: 'সরিষা',
+  ),
+  CorpModel(
+    id: 10,
+    name: 'পেঁয়াজ',
+  ),
+  CorpModel(
+    id: 11,
+    name: 'রসুন',
+  ),
+  CorpModel(
+    id: 12,
+    name: 'আম',
+  ),
+  CorpModel(
+    id: 13,
+    name: 'কলা',
+  ),
+  CorpModel(
+    id: 14,
+    name: 'আনারস',
+  ),
+  CorpModel(
+    id: 15,
+    name: 'কাঁঠাল',
+  ),
+  CorpModel(
+    id: 16,
+    name: 'মরিচ',
+  ),
+  CorpModel(
+    id: 17,
+    name: 'ফুলকপি',
+  ),
+  CorpModel(
+    id: 18,
+    name: 'বাঁধাকপি',
+  ),
+  CorpModel(
+    id: 19,
+    name: 'মিষ্টি কুমড়া',
+  ),
+  CorpModel(
+    id: 20,
+    name: 'লাউ',
+  ),
+  CorpModel(
+    id: 21,
+    name: 'তরমুজ',
+  ),
+  CorpModel(
+    id: 22,
+    name: 'পটল',
+  ),
+  CorpModel(
+    id: 23,
+    name: 'বরবটি',
+  ),
+  CorpModel(
+    id: 24,
+    name: 'কচু',
+  ),
+  CorpModel(
+    id: 25,
+    name: 'ঢেঁরস',
+  ),
+  CorpModel(
+    id: 26,
+    name: 'চাল কুমড়া',
+  ),
+  CorpModel(
+    id: 27,
+    name: 'ঝিঙ্গা',
+  ),
+  CorpModel(
+    id: 28,
+    name: 'চিচিঙ্গা',
+  ),
+  CorpModel(
+    id: 29,
+    name: 'ধুন্দল',
+  ),
+  CorpModel(
+    id: 30,
+    name: 'কাঁকরোল',
+  ),
+  CorpModel(
+    id: 31,
+    name: 'করলা',
+  ),
+  CorpModel(
+    id: 32,
+    name: 'সজনে',
+  ),
+  CorpModel(
+    id: 33,
+    name: 'বেগুন',
+  ),
+  CorpModel(
+    id: 34,
+    name: 'পেঁপে',
+  ),
+  CorpModel(
+    id: 35,
+    name: 'আদা',
+  ),
+  CorpModel(
+    id: 36,
+    name: 'হলুদ',
+  ),
+  CorpModel(
+    id: 37,
+    name: 'লালশাক',
+  ),
+  CorpModel(
+    id: 38,
+    name: 'পুইশাক',
+  ),
+  CorpModel(
+    id: 39,
+    name: 'ডাটা শাক',
+  ),
+  CorpModel(
+    id: 40,
+    name: 'পালংশাক',
+  ),
+  CorpModel(
+    id: 41,
+    name: 'কলমিশাক',
+  ),
+  CorpModel(
+    id: 42,
+    name: 'ক্যাপসিকাম',
+  ),
+  CorpModel(
+    id: 43,
+    name: 'বাটিশাক',
+  ),
+  CorpModel(
+    id: 44,
+    name: 'লেটুস পাতা',
+  ),
+];
