c2d7e0e3277c53edde6e8ee724c20869d0e2b7a2
YamazakiKunq <yamazakik447@gmail.com>
2023-08-01 17:55:59 +0600

    Division form field dropdown UI

---
+++ lib/presentation/pages/add_farmer/components/division_form_field.dart
@@ -0,0 +1,50 @@
+import 'package:deshifarmer/data/datasources/local/location_db/dist_db.dart';
+import 'package:deshifarmer/data/models/location_model.dart';
+import 'package:flutter/material.dart';
+
+class DivisionFormField extends StatelessWidget {
+  const DivisionFormField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: DropdownButtonFormField<DistModel>(
+        borderRadius: const BorderRadius.all(Radius.circular(15)),
+        isExpanded: true,
+        decoration: const InputDecoration(
+          label: Text('জেলা নির্বাচন করুন'),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(
+              width: 2,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(width: 2),
+          ),
+          filled: true,
+          // fillColor: Colors.greenAccent,
+          // contentPadding: EdgeInsets.all(10),
+        ),
+        // decoration: ShapeDecoration(),
+
+        elevation: 16,
+        value: distDatabase.first,
+        items: distDatabase.map<DropdownMenuItem<DistModel>>((DistModel value) {
+          return DropdownMenuItem<DistModel>(
+            // alignment: Alignment.center,
+            value: value,
+            child: Text(value.bnName),
+          );
+        }).toList(),
+        onChanged: (DistModel? val) {
+          // context.read<DropdownCubit>().changeDropdownValue(val ?? '');
+        },
+      ),
+    );
+  }
+}
