a39b1bb542bd7c4a500206517c07ccdc8407c1e9
RomjanHossain <romjan@skiff.com>
2023-08-02 17:52:06 +0600

    get group states from API

---
+++ lib/presentation/cubit/groups/get_group_state.dart
@@ -0,0 +1,10 @@
+part of 'get_group_cubit.dart';
+
+abstract class GetGroupState extends Equatable {
+  const GetGroupState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetGroupInitial extends GetGroupState {}
