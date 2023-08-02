ce191a2852acb2d1ae22e54e49f463b1f4dabead
YamazakiKunq <yamazakik447@gmail.com>
2023-08-02 17:56:17 +0600

    farmer Date of Birth field added

--- lib/presentation/pages/add_farmer/components/farmer_dob_field.dart
+++ lib/presentation/pages/add_farmer/components/farmer_dob_field.dart
@@ -1,3 +1,4 @@
+import 'package:deshifarmer/presentation/pages/add_farmer/add_farmer.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -12,13 +13,14 @@ class FarmarDOB extends StatefulWidget {
 
 class _FarmarDOBState extends State<FarmarDOB> {
   DateTime? _dob = DateTime.now();
-  TextEditingController textEditingController = TextEditingController();
+  //TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    final state = context.read<AddFarmerBloc>().state;
     return Padding(
       padding: const EdgeInsets.all(8),
       child: TextField(
-        controller: textEditingController,
+        controller: state is AddFarmerInitial ? state.dobController : null,
         onTap: () async {
           _dob = await showDatePicker(
             context: context,
@@ -27,12 +29,16 @@ class _FarmarDOBState extends State<FarmarDOB> {
             lastDate: DateTime.now(),
           );
           if (_dob != null) {
-            textEditingController.text = DateFormat('yyyy-MM-dd').format(_dob!);
+            if (state is AddFarmerInitial) {
+              state.dobController.text = DateFormat('yyyy-MM-dd').format(_dob!);
+            }
           }
         },
         keyboardType: TextInputType.none,
         decoration: const InputDecoration(
-          border: OutlineInputBorder(),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+          ),
           labelText: 'কৃষকের জন্ম তারিখ',
           hintText: '',
           filled: true,
