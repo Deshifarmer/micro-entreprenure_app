d92a327fd5ae8526ceaf88b733d14901c729f489
RomjanHossain <romjan@skiff.com>
2023-08-05 15:37:27 +0600

    farmer focused/current focused fixed - close #1

---
+++ lib/presentation/pages/add_farmer/components/famer_focused_corp_multi.dart
@@ -0,0 +1,131 @@
+import 'package:deshifarmer/data/datasources/local/corps/corps_db.dart';
+import 'package:deshifarmer/presentation/pages/add_farmer/add_farmer.dart';
+import 'package:deshifarmer/presentation/pages/add_farmer/bloc/add_farmer_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:multi_dropdown/multiselect_dropdown.dart';
+
+class FarmerFocusedCorpMulti extends StatelessWidget {
+  const FarmerFocusedCorpMulti({
+    super.key,
+  });
+
+  // final MultiSelectController _multiSelectController = MultiSelectController();
+  @override
+  Widget build(BuildContext context) {
+    // print('multi values from state -> $_cropsList');
+    // final state = context.read<AddFarmerBloc>().state;
+    return BlocConsumer<AddFarmerBloc, AddFarmerState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        if (state is AddFarmerInitial) {
+          final newF = state.farmerFocusedCorpController.text
+              .replaceAll('{', '')
+              .replaceAll('}', '')
+              .split(',');
+          final vls = newF.map((val) {
+            return ValueItem(label: val);
+          }).toList();
+          if (vls.length == 1 && vls.first.label.isEmpty) {
+            vls.removeLast();
+          }
+          return Padding(
+            padding: const EdgeInsets.all(8),
+            child: MultiSelectDropDown(
+              // controller: _multiSelectController,
+              hint: 'প্রধান ফসল নির্বাচন',
+              selectedOptions: vls,
+              // selectedOptions:state.farmerFocusedCorpController.text ,
+              chipConfig: ChipConfig(
+                wrapType: WrapType.wrap,
+                backgroundColor: Colors.green[400],
+              ),
+              selectedOptionTextColor: Colors.green,
+              options:
+                  cropsDatabase.map((e) => ValueItem(label: e.name)).toList(),
+              onOptionSelected: (selectedOptions) {
+                // print(
+                //     'multi select controller -> ${_multiSelectController.selectedOptions}');
+                final values = [];
+                // print(selectedOptions);
+                for (final vi in selectedOptions) {
+                  // print(vi.label);
+                  values.add(vi.label);
+                }
+
+                state.farmerFocusedCorpController.text =
+                    values.toSet().toString();
+              },
+              padding: const EdgeInsets.all(8),
+              dropdownHeight: MediaQuery.of(context).size.height / 2.5,
+            ),
+          );
+        }
+        return const SizedBox.shrink();
+      },
+    );
+  }
+}
+
+class FarmerCurrentProducingCorp extends StatelessWidget {
+  const FarmerCurrentProducingCorp({
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    // print('multi values from state -> $_cropsList');
+    // final state = context.read<AddFarmerBloc>().state;
+    return BlocConsumer<AddFarmerBloc, AddFarmerState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        if (state is AddFarmerInitial) {
+          final newF = state.farmerCurrentProducingCorpController.text
+              .replaceAll('{', '')
+              .replaceAll('}', '')
+              .split(',');
+
+          print('new f ${newF.length} ${newF.first}');
+          final vls = newF.map(
+            (val) {
+              return ValueItem(label: val);
+            },
+          ).toList();
+          if (vls.length == 1 && vls.first.label.isEmpty) {
+            vls.removeLast();
+          }
+          return Padding(
+            padding: const EdgeInsets.all(8),
+            child: MultiSelectDropDown(
+              // controller: _multiSelectController,
+              hint: 'বর্তমান উৎপাদনকারী ফসল নির্বাচন করুন',
+              selectedOptions: vls,
+              // selectedOptions:state.farmerFocusedCorpController.text ,
+              chipConfig: ChipConfig(
+                wrapType: WrapType.wrap,
+                backgroundColor: Colors.green[400],
+              ),
+              selectedOptionTextColor: Colors.green,
+              options:
+                  cropsDatabase.map((e) => ValueItem(label: e.name)).toList(),
+              onOptionSelected: (selectedOptions) {
+                // print(
+                //     'multi select controller -> ${_multiSelectController.selectedOptions}');
+                final values = [];
+                // print(selectedOptions);
+                for (final vi in selectedOptions) {
+                  // print(vi.label);
+                  values.add(vi.label);
+                }
+
+                state.farmerCurrentProducingCorpController.text =
+                    values.toSet().toString();
+              },
+              padding: const EdgeInsets.all(8),
+              dropdownHeight: MediaQuery.of(context).size.height / 2.5,
+            ),
+          );
+        }
+        return const SizedBox.shrink();
+      },
+    );
+  }
+}
