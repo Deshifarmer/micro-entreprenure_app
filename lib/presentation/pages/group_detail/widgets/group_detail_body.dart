6beadf26a4d337f83174351240cc0326745fc0cf
YamazakiKunq <yamazakik447@gmail.com>
2023-08-08 12:26:08 +0600

    remove more code for less

--- lib/presentation/pages/group_detail/widgets/group_detail_body.dart
+++ lib/presentation/pages/group_detail/widgets/group_detail_body.dart
@@ -1,13 +1,13 @@
 import 'package:deshifarmer/core/app_core.dart';
-import 'package:deshifarmer/core/app_strings.dart';
 import 'package:deshifarmer/data/datasources/remote/apis/api_source.dart';
 import 'package:deshifarmer/domain/entities/farmer_entity/farmer_entity.dart';
-import 'package:deshifarmer/presentation/blocs/my_unassign_farmers/my_unassign_famers_bloc.dart';
 import 'package:deshifarmer/presentation/cubit/add_group/add_farmer_to_group_cubit.dart';
 import 'package:deshifarmer/presentation/pages/add_farmer/add_farmer.dart';
 import 'package:deshifarmer/presentation/pages/group_detail/bloc/group_detail_bloc.dart';
+import 'package:deshifarmer/presentation/pages/group_detail/components/add_farmer_to_gc.dart';
 import 'package:deshifarmer/presentation/pages/group_detail/components/group_detail_card.dart';
 import 'package:deshifarmer/presentation/pages/login/bloc/login_bloc.dart';
+import 'package:deshifarmer/presentation/widgets/farmer_card.dart';
 import 'package:deshifarmer/presentation/widgets/primary_btn.dart';
 import 'package:flutter/material.dart';
 
@@ -233,26 +233,7 @@ class GroupDetailBody extends StatelessWidget {
                 itemBuilder: (context, index) {
                   var currentFarmer =
                       state.groupDetailEntity.farmer_list?.elementAt(index);
-                  return Card(
-                    child: ListTile(
-                      leading: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          '${Strings.domain}/storage/${currentFarmer?.image}',
-                          height: 50,
-                          width: 50,
-                        ),
-                      ),
-                      title: Text(currentFarmer?.full_name ?? ''),
-                      trailing: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(currentFarmer?.phone ?? ''),
-                          Text(currentFarmer?.district ?? ''),
-                        ],
-                      ),
-                    ),
-                  );
+                  return FarmerCard(currentFarmer: currentFarmer);
                 },
               ),
             ],
@@ -261,89 +242,5 @@ class GroupDetailBody extends StatelessWidget {
         return SizedBox.shrink();
       },
     );
-  }
-}
-
-/// TODO: Assigned Farmes in the Group api
-/// TODO: Group leader Select API
-
-class AddFarmerToTheAGroup extends StatelessWidget {
-  const AddFarmerToTheAGroup({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<MyUnassignFamersBloc, MyUnassignFamersState>(
-      listener: (context, state) {
-        // TODO: implement listener
-      },
-      builder: (context, state) {
-        if (state is MyUnassignFarmerReqSuccess) {
-          return Padding(
-            padding: const EdgeInsets.all(8),
-            child: DropdownButtonFormField<FarmerEntity>(
-              // padding: const EdgeInsets.all(10),
-              // menuMaxHeight: 80,
-              isDense: false,
-              // itemHeight: 100,
-              borderRadius: const BorderRadius.all(Radius.circular(15)),
-              isExpanded: true,
-              decoration: const InputDecoration(
-                label: Text(('কৃষক সিলেক্ট করুন')),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                  borderSide: BorderSide(
-                    width: 2,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  borderSide: BorderSide(width: 2),
-                  gapPadding: 10,
-                ),
-                filled: true,
-              ),
-              // decoration: ShapeDecoration(),
-              // itemHeight: 300,
-              elevation: 16,
-              value: state.allFarmerListResp.farmers.first,
-              // value: state.allFarmerListResp.farmers.first,
-              items: state.allFarmerListResp.farmers
-                  .map<DropdownMenuItem<FarmerEntity>>((value) {
-                return DropdownMenuItem<FarmerEntity>(
-                  alignment: Alignment.center,
-                  value: value,
-                  child: ListTile(
-                    leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.network(
-                        '${Strings.domain}/storage/${value.image}',
-                        height: 50,
-                        width: 50,
-                      ),
-                    ),
-                    title: Text(value.full_name ?? ''),
-                    subtitle: Text(value.phone ?? ''),
-                  ),
-                );
-              }).toList(),
-              onChanged: (FarmerEntity? val) {
-                if (val?.farmer_id != null) {
-                  context
-                      .read<AddFarmerToGroupCubit>()
-                      .addFarmer(val?.farmer_id ?? '');
-                  print('farmer id -> ${val?.farmer_id}');
-                }
-              },
-            ),
-          );
-        }
-
-        return Text('no mfarjk');
-      },
-    );
-
-    // return const Placeholder();
   }
 }
