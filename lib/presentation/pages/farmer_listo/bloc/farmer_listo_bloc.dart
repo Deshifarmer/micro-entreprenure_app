9271d5a511e0c166b775a0b33abdfd4a550aa998
YamazakiKunq <yamazakik447@gmail.com>
2023-08-08 12:24:44 +0600

    farmer listo BLOC

---
+++ lib/presentation/pages/farmer_listo/bloc/farmer_listo_bloc.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+part 'farmer_listo_event.dart';
+part 'farmer_listo_state.dart';
+
+class FarmerListoBloc extends Bloc<FarmerListoEvent, FarmerListoState> {
+  FarmerListoBloc() : super(const FarmerListoInitial()) {
+    on<CustomFarmerListoEvent>(_onCustomFarmerListoEvent);
+  }
+
+  FutureOr<void> _onCustomFarmerListoEvent(
+    CustomFarmerListoEvent event,
+    Emitter<FarmerListoState> emit,
+  ) {
+    // TODO: Add Logic
+  }
+}
