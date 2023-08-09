479396c78d63a675eafc25f051297fd248c98176
YamazakiKunq <yamazakik447@gmail.com>
2023-08-09 15:20:13 +0600

    home page icon model

---
+++ lib/data/models/home_page_icon_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/rendering.dart';
+
+class HomeIconModel {
+  HomeIconModel({
+    required this.painter,
+    required this.title,
+  });
+  final String title;
+  final CustomPainter painter;
+}
