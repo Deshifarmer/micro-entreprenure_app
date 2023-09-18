9f59bc96ed02ba0883ef606062c76267a6158f24
RomjanHossain <romjan@skiff.com>
2023-09-18 12:32:15 +0600

    farmer fetch farm event

---
+++ lib/presentation/blocs/farmer_fetch_farm/farmer_fetch_farm_event.dart
@@ -0,0 +1,17 @@
+part of 'farmer_fetch_farm_bloc.dart';
+
+sealed class FarmerFetchFarmEvent extends Equatable {
+  const FarmerFetchFarmEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class FramFetchEvent extends FarmerFetchFarmEvent {
+  const FramFetchEvent({
+    required this.token,
+    required this.farmerID,
+  });
+  final String token;
+  final String farmerID;
+}
