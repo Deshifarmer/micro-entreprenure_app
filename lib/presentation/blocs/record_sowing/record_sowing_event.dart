60fcab7b9f6eb3231bce3358240c04fb502ccffe
RomjanHossain <romjan@skiff.com>
2023-09-18 12:32:57 +0600

    record sowing event

---
+++ lib/presentation/blocs/record_sowing/record_sowing_event.dart
@@ -0,0 +1,13 @@
+part of 'record_sowing_bloc.dart';
+
+sealed class RecordSowingEvent extends Equatable {
+  const RecordSowingEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class RecordSowingPostEvent extends RecordSowingEvent {
+  const RecordSowingPostEvent(this.ram);
+  final RecordActivityModel ram;
+}
