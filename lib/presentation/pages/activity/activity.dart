b89ebb642a65fcdaa0ca2c5b78caae70668978f6
RomjanHossain <romjanhossain726526@gmail.com>
2023-09-18 12:33:35 +0600

    activity bloc, state and event

---
+++ lib/presentation/pages/activity/activity.dart
@@ -0,0 +1,3 @@
+export 'bloc/bloc.dart';
+export 'view/activity_page.dart';
+export 'widgets/widgets.dart';
---
+++ lib/presentation/pages/activity/bloc/activity_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+part 'activity_event.dart';
+part 'activity_state.dart';
+
+class ActivityBloc extends Bloc<ActivityEvent, ActivityState> {
+  ActivityBloc() : super(ActivityInitial()) {
+    on<CustomActivityEvent>(_onCustomActivityEvent);
+  }
+
+  FutureOr<void> _onCustomActivityEvent(
+    CustomActivityEvent event,
+    Emitter<ActivityState> emit,
+  ) {
+    // TODO: Add Logic
+  }
+}
