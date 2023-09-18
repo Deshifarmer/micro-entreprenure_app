63bcb3b160243fa620e6e9a6a4df87287df926c2
RomjanHossain <romjan@skiff.com>
2023-09-18 12:58:05 +0600

    profile details update

---
+++ lib/presentation/pages/profile/pages/details_update.dart
@@ -0,0 +1,206 @@
+import 'package:deshifarmer/core/app_strings.dart';
+import 'package:deshifarmer/presentation/utils/deshi_colors.dart';
+import 'package:deshifarmer/presentation/widgets/constraints.dart';
+import 'package:deshifarmer/presentation/widgets/seconday_btn.dart';
+import 'package:flutter/material.dart';
+
+class ProfileUpdatePage extends StatelessWidget {
+  const ProfileUpdatePage({
+    required this.fullName,
+    required this.farmerID,
+    required this.farmerPhoto,
+    super.key,
+  });
+
+  final String? fullName;
+  final String? farmerID;
+  final String? farmerPhoto;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+          CircleAvatar(
+            radius: 50,
+            backgroundColor: Colors.transparent,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                checkDomain(Strings.getServerOrLocal(ServerOrLocal.server))
+                    ? dummyImage
+                    : '${Strings.getServerOrLocal(ServerOrLocal.server)}/storage/$farmerPhoto',
+                fit: BoxFit.cover,
+                /* errorBuilder: (
+                        context,
+                        error,
+                        stackTrace,
+                      ) {
+                        return Center(
+                          child: Text(
+                            'Image Error',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .copyWith(
+                                  color: Colors.redAccent,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                          ),
+                        );
+ss                       }, */
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              fullName ?? '',
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+
+          /// code
+          Text(
+            '#$farmerID',
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+
+          Container(
+            padding: const EdgeInsets.all(8),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 20,
+            ),
+            decoration: BoxDecoration(
+              color: tertiaryColor2,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            height: 40,
+            // width: getProportionateScreenWidth(60),
+            width: MediaQuery.sizeOf(context).width / 1.2,
+            child: const Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: priceBoxColor,
+                  // radius: 30,
+                  child: Icon(Icons.check, size: 15),
+                ),
+                Expanded(
+                  child: Text(
+                    'সকল তথ্য আপ টু ডেট রয়েছে ',
+                    // 'No information update is required at this moment',
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                    softWrap: true,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: getProportionateScreenHeight(40),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            child: TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                // fillColor: backgroundColor2,
+                hintText: 'Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  borderSide: BorderSide.none,
+                ),
+
+                contentPadding: EdgeInsets.symmetric(
+                  // vertical: 20,
+                  horizontal: 15,
+                ),
+                filled: true,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            child: TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                // fillColor: backgroundColor2,
+                hintText: 'Address',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: EdgeInsets.symmetric(
+                  // vertical: 20,
+                  horizontal: 15,
+                ),
+                filled: true,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            child: TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                // fillColor: backgroundColor2,
+                hintText: 'Select group Leader',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: EdgeInsets.symmetric(
+                  // vertical: 20,
+                  horizontal: 15,
+                ),
+                filled: true,
+              ),
+            ),
+          ),
+
+          // Text('আমরা আপনাকে কীভাবে সাহায্য করতে পারি  ?'),
+        ],
+      ),
+      bottomNavigationBar: SecondayButtonGreen(
+        onpress: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              backgroundColor: priceBoxColor,
+              content: const Text('This is Under progress'),
+              action: SnackBarAction(label: 'Okay', onPressed: () {}),
+            ),
+          );
+        },
+        title: 'Update My Detail',
+      ),
+    );
+  }
+}
