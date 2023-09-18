e6666971e6afe583278e7a870400487335e15b3e
RomjanHossain <romjan@skiff.com>
2023-09-18 12:39:22 +0600

    primary loading deshifarmer

---
+++ lib/presentation/widgets/primary_loading_progress.dart
@@ -0,0 +1,14 @@
+// CircularProgressIndicator(
+//                     color: Colors.green[600],
+//                   )
+
+import 'package:flutter/material.dart';
+
+class PrimaryLoadingIndicator extends StatelessWidget {
+  const PrimaryLoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) => CircularProgressIndicator(
+        color: Colors.green[600],
+      );
+}
