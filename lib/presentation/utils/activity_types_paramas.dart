012b18e5ae943c6729a456b727165e4b9ad701c7
RomjanHossain <romjan@skiff.com>
2023-09-18 12:46:11 +0600

    activity type parameters

---
+++ lib/presentation/utils/activity_types_paramas.dart
@@ -0,0 +1,13 @@
+enum ActivityTypeEnums {
+  landpref,
+  sowing,
+  irrigation,
+  weeding,
+}
+
+Map<String, ActivityTypeEnums> activityRecordValues = {
+  'Land Preparation': ActivityTypeEnums.landpref,
+  'Sowing': ActivityTypeEnums.sowing,
+  'Irrigation': ActivityTypeEnums.irrigation,
+  'Weedign': ActivityTypeEnums.weeding,
+};
