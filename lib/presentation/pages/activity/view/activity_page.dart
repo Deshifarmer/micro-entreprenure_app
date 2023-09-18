f82404c37390e5ec10f387c319093d4dd2a2b111
RomjanHossain <romjan@skiff.com>
2023-09-18 13:05:43 +0600

    activity page and body (functional)

---
+++ lib/presentation/pages/activity/view/activity_page.dart
@@ -0,0 +1,69 @@
+import 'package:deshifarmer/presentation/animations/page_animations.dart';
+import 'package:deshifarmer/presentation/pages/activity/bloc/bloc.dart';
+import 'package:deshifarmer/presentation/pages/activity/pages/activity_select_farm.dart';
+import 'package:deshifarmer/presentation/pages/activity/widgets/activity_body.dart';
+import 'package:deshifarmer/presentation/utils/deshi_colors.dart';
+import 'package:deshifarmer/presentation/widgets/seconday_btn.dart';
+import 'package:flutter/material.dart';
+
+/// {@template activity_page}
+/// A description for ActivityPage
+/// {@endtemplate}
+class ActivityPage extends StatelessWidget {
+  /// {@macro activity_page}
+  const ActivityPage({super.key});
+
+  /// The static route for ActivityPage
+  static Route<dynamic> route() {
+    return MaterialPageRoute<dynamic>(builder: (_) => const ActivityPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ActivityBloc(),
+      child: Scaffold(
+        backgroundColor: backgroundColor2,
+        appBar: AppBar(
+          backgroundColor: backgroundColor2,
+          title: Text('সাম্প্রতিক কার্যকলাপ '),
+          // toolbarHeight: 30,
+          surfaceTintColor: backgroundColor2,
+        ),
+        body: const ActivityView(),
+        extendBody: true,
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+          ),
+          child: SecondayButtonGreen(
+            onpress: () {
+              Navigator.push(
+                context,
+                PageAnimationBottom2Up(
+                  const ActivityAddFarm(),
+                ),
+              );
+            },
+            title: 'নতুন কার্যকলাপ ট্র্যাক করুন ',
+            // title: 'record new activity',
+            btnColor: priceBoxColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// {@template activity_view}
+/// Displays the Body of ActivityView
+/// {@endtemplate}
+class ActivityView extends StatelessWidget {
+  /// {@macro activity_view}
+  const ActivityView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ActivityBody();
+  }
+}
