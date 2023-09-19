a9dbda48afb83493225b23594caacf5fbe2d2a34
RomjanHossain <romjanhossain726526@gmail.com>
2023-09-19 18:00:09 +0600

    new bottom nav

--- lib/presentation/pages/home/components/home_bottom_nav.dart
+++ lib/presentation/pages/home/components/home_bottom_nav.dart
@@ -3,6 +3,7 @@ import 'package:deshifarmer/presentation/pages/home/bloc/bloc.dart';
 import 'package:deshifarmer/presentation/pages/login/bloc/bloc.dart';
 import 'package:deshifarmer/presentation/pages/order/order.dart';
 import 'package:deshifarmer/presentation/utils/deshi_colors.dart';
+import 'package:deshifarmer/presentation/widgets/size_config.dart';
 import 'package:flutter/material.dart';
 
 /// home page bottom navigation bar
@@ -10,11 +11,17 @@ class HomeBottomNav extends StatelessWidget {
   const HomeBottomNav({
     super.key,
   });
-
+  final icons = const <IconData>[
+    Icons.view_carousel_outlined,
+    Icons.cake_outlined,
+    Icons.reorder,
+    Icons.account_circle_outlined,
+  ];
   @override
   Widget build(BuildContext context) {
     final state = context.watch<HomeBloc>().state;
-    return AnimatedBottomNavigationBar(
+    return AnimatedBottomNavigationBar.builder(
+      height: getProportionateScreenHeight(70),
       activeIndex: state is HomeInitial
           ? 0
           : state is MyOrderPageState
@@ -32,12 +39,45 @@ class HomeBottomNav extends StatelessWidget {
       blurEffect: true,
       hideAnimationCurve: Curves.easeOut,
       elevation: 0,
-      icons: const <IconData>[
-        Icons.view_carousel_outlined,
-        Icons.cake_outlined,
-        Icons.reorder,
-        Icons.account_circle_outlined,
-      ],
+      // icons: const <IconData>[
+      //   Icons.view_carousel_outlined,
+      //   Icons.cake_outlined,
+      //   Icons.reorder,
+      //   Icons.account_circle_outlined,
+      // ],
+      itemCount: 4,
+      tabBuilder: (index, isActive) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icons[index],
+              // size: 24,
+              color: isActive ? Colors.white : Colors.grey,
+            ),
+            const SizedBox(height: 4),
+            Text(
+              index == 0
+                  ? 'হোম'
+                  : index == 1
+                      ? 'কৃষক তালিকা'
+                      : index == 2
+                          ? 'অর্ডার তালিকা'
+                          : 'প্রোফাইল',
+              // style: TextStyle(
+              //   color: isActive ? Colors.white : Colors.grey,
+              //   // fontSize: 12,
+              //   fontWeight: FontWeight.w600,
+              // ),
+              style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                    color: isActive ? Colors.white : Colors.grey,
+                    // fontWeight: FontWeight.w600,
+                  ),
+            ),
+          ],
+        );
+      },
       onTap: (int x) {
         final loginState = context.read<LoginBloc>().state;
         if (x == 1) {
@@ -55,8 +95,8 @@ class HomeBottomNav extends StatelessWidget {
         context.read<HomeBloc>().add(ChangePageEvent(x));
       },
       backgroundColor: tertiaryColor,
-      activeColor: Colors.white,
-      inactiveColor: Colors.grey,
+      // activeColor: Colors.white,
+      // inactiveColor: Colors.grey,
     );
   }
 }
