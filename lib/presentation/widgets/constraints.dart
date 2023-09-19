b36c9ac1cf06bf1ce0a29d696ad1bf52425d61fb
RomjanHossain <romjan@skiff.com>
2023-09-19 13:01:23 +0600

    remove the size config from consts

--- lib/presentation/widgets/constraints.dart
+++ lib/presentation/widgets/constraints.dart
@@ -1,19 +1,5 @@
+import 'package:deshifarmer/presentation/widgets/size_config.dart';
 import 'package:flutter/material.dart';
-
-class SizeConfig {
-  static late MediaQueryData _mediaQueryData;
-  static late double screenWidth;
-  static late double screenHeight;
-  static double? defaultSize;
-  static Orientation? orientation;
-
-  void init(BuildContext context) {
-    _mediaQueryData = MediaQuery.of(context);
-    screenWidth = _mediaQueryData.size.width;
-    screenHeight = _mediaQueryData.size.height;
-    orientation = _mediaQueryData.orientation;
-  }
-}
 
 const String dummyImage =
     'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png';
@@ -34,20 +20,6 @@ List<String> soilTypes = [
   'Clay soil(কাঁদা মাটি)',
   'Alluvial soil(পলি মাটি)',
 ];
-
-// Get the proportionate height as per screen size
-double getProportionateScreenHeight(double inputHeight) {
-  final screenHeight = SizeConfig.screenHeight;
-  // 812 is the layout height that designer use
-  return (inputHeight / 812.0) * screenHeight;
-}
-
-// Get the proportionate height as per screen size
-double getProportionateScreenWidth(double inputWidth) {
-  final screenWidth = SizeConfig.screenWidth;
-  // 375 is the layout width that designer use
-  return (inputWidth / 375.0) * screenWidth;
-}
 
 ///! HACK: Font sizes
 const double normalFont = 14;
