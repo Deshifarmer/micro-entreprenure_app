5c930a3cdad703e2be9b97315353ad47364c569e
RomjanHossain <romjanhossain726526@gmail.com>
2023-09-21 16:01:53 +0600

    optional icon or custompainter

--- lib/data/models/home_page_icon_model.dart
+++ lib/data/models/home_page_icon_model.dart
@@ -1,10 +1,12 @@
-import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeIconModel {
   HomeIconModel({
-    required this.painter,
     required this.title,
+    this.painter,
+    this.icon,
   });
   final String title;
-  final CustomPainter painter;
+  final CustomPainter? painter;
+  final Icon? icon;
 }
