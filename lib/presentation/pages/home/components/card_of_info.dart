2bfcc8b3a76d59dc2ecfabefe2aa8ec157a7875b
RomjanHossain <romjanhossain726526@gmail.com>
2023-09-21 16:02:55 +0600

    remove padding

--- lib/presentation/pages/home/components/card_of_info.dart
+++ lib/presentation/pages/home/components/card_of_info.dart
@@ -17,62 +17,59 @@ class CardOfSmallInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: Card(
-        color: tertiaryColor,
-        child: Row(
-          // mainAxisAlignment: MainAxisAlignment.c,
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: getProportionateScreenWidth(10)),
-              // child: Icon(
-              //   icon,
-              //   // size: 25,
-              //   color: Colors.white,
-              // ),
-              child: isIcon
-                  ? Icon(
-                      Icons.format_list_numbered,
-                      color: Colors.white,
-                    )
-                  : CustomPaint(
-                      painter: const MyFarmersShape(
-                        colorF: Colors.white,
-                      ),
-                      size: Size(
-                        getProportionateScreenWidth(30),
-                        getProportionateScreenHeight(30),
-                      ),
-                      // size: 25,
-                      // color: Colors.white,
+    return Card(
+      color: tertiaryColor,
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.c,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(10)),
+            // child: Icon(
+            //   icon,
+            //   // size: 25,
+            //   color: Colors.white,
+            // ),
+            child: isIcon
+                ? Icon(
+                    Icons.format_list_numbered,
+                    color: Colors.white,
+                  )
+                : CustomPaint(
+                    painter: const MyFarmersShape(
+                      colorF: Colors.white,
                     ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                        color: Colors.white,
-                        letterSpacing: .9,
-                        // fontWeight: FontWeight.w500,
-                      ),
-                ),
-                Text(
-                  ammount,
-                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                    size: Size(
+                      getProportionateScreenWidth(30),
+                      getProportionateScreenHeight(30),
+                    ),
+                    // size: 25,
+                    // color: Colors.white,
+                  ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: Colors.white,
+                      letterSpacing: .9,
+                      // fontWeight: FontWeight.w500,
+                    ),
+              ),
+              Text(
+                ammount,
+                style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
