bc59f8ca1b2de09cba192d5d7527b41c740b9018
RomjanHossain <romjan@skiff.com>
2023-09-23 16:26:31 +0600

    asking for msg, lat and long

--- lib/presentation/pages/attendance/bloc/attendance_event.dart
+++ lib/presentation/pages/attendance/bloc/attendance_event.dart
@@ -30,14 +30,19 @@ class CheckOutEvent extends AttendanceEvent {
     required this.token,
     required this.id,
     required this.imageFile,
+    required this.lat,
+    required this.long,
+    this.msg,
   });
   final String imageFile;
   final String token;
   final String id;
+  final String? msg;
+  final double lat;
+  final double long;
 }
 
 /// FIX:check in success from futurebuilder
-
 class CheckInFromFuture extends AttendanceEvent {
   const CheckInFromFuture(this.today);
 
