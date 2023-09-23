33e2e7186ce9da067bd442556137f718637fe447
RomjanHossain <romjan@skiff.com>
2023-09-23 16:26:15 +0600

    so mancy chnages in the att bloc

--- lib/presentation/pages/attendance/bloc/attendance_bloc.dart
+++ lib/presentation/pages/attendance/bloc/attendance_bloc.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:deshifarmer/data/datasources/remote/apis/api_source.dart';
+import 'package:deshifarmer/core/usecase/location_current.dart';
+import 'package:deshifarmer/data/datasources/remote/apis/attendance_api.dart';
 import 'package:deshifarmer/domain/entities/attendance/todays_resp_entity.dart';
 import 'package:equatable/equatable.dart';
 part 'attendance_event.dart';
@@ -15,7 +16,7 @@ class AttendanceBloc extends Bloc<AttendanceEvent, AttendanceState> {
     on<CheckInFromFuture>(_onCheckInFromFuture);
   }
 
-  DeshiFarmerAPI deshiFarmerAPI = DeshiFarmerAPI();
+  AttendanceAPI deshiFarmerAPI = AttendanceAPI();
 
   FutureOr<void> _onCustomAttendanceEvent(
     CustomAttendanceEvent event,
@@ -26,20 +27,22 @@ class AttendanceBloc extends Bloc<AttendanceEvent, AttendanceState> {
   FutureOr<void> _onCheckOutEvent(CheckOutEvent event, emit) async {
     emit(AttendanceLoading());
 
-    ///! TODO: uncomment this
-    // final todaysAtten = await deshiFarmerAPI.checkOut(
-    //   event.token,
-    //   event.id,
-    //   event.imageFile,
-    // );
-    // if (todaysAtten.$1) {
-    //   emit(AttendanceOut());
+    final todaysAtten = await deshiFarmerAPI.checkOut(
+      event.token,
+      event.id,
+      event.msg,
+      event.imageFile,
+      event.lat,
+      event.long,
+    );
+    if (todaysAtten.$1) {
+      emit(AttendanceOut());
 
-    //   emit(const AttendanceInitial());
-    // } else {
-    //   emit(AttendanceError());
-    //   emit(const AttendanceInitial());
-    // }
+      emit(const AttendanceInitial());
+    } else {
+      emit(AttendanceError());
+      emit(const AttendanceInitial());
+    }
   }
 
   /// PERF: Check in
@@ -47,43 +50,42 @@ class AttendanceBloc extends Bloc<AttendanceEvent, AttendanceState> {
     emit(AttendanceLoading());
 
     ///! TODO: uncomment this
-    // final todaysAtten = await deshiFarmerAPI.getTodaysAttendance(event.token);
-    // print('this is att from BLOCK ${todaysAtten == null} -> $todaysAtten');
-    // if (todaysAtten != null) {
-    //   // print('BLOCK att is not NULL');
-    //   print('todays attendance From BLOC ${todaysAtten}');
-    //   emit(AttendanceSuccess(todays: todaysAtten));
-    // } else {
-    //   // print('else is running');
-    //   try {
-    //     // print('trying get the location');
-    //     final getCurrentPosition = await determinePosition();
+    final todaysAtten = await deshiFarmerAPI.getTodaysAttendance(event.token);
 
-    //     final checkingIN = await deshiFarmerAPI.checkInAPI(
-    //       event.token,
-    //       null,
-    //       event.imageFile,
-    //       getCurrentPosition.latitude,
-    //       getCurrentPosition.longitude,
-    //     );
+    if (todaysAtten != null) {
+      // print('BLOCK att is not NULL');
+      emit(AttendanceSuccess(todays: todaysAtten));
+    } else {
+      // print('else is running');
+      try {
+        // print('trying get the location');
+        final getCurrentPosition = await determinePosition();
 
-    //     // print('check in from BLOCK -> ${checkingIN.$1} : ${checkingIN.$2}');
-    //     if (checkingIN.$1) {
-    //       final todaysAtten =
-    //           await deshiFarmerAPI.getTodaysAttendance(event.token);
-    //       print('todays attendance From BLOC ${todaysAtten}');
-    //       if (todaysAtten != null) {
-    //         emit(AttendanceSuccess(todays: todaysAtten));
-    //       }
-    //     }
-    //   } catch (e) {
-    //     print('error happend on getting location $e');
-    //     emit(AttendanceError(msg: e.toString()));
-    //   }
-    //   // print('')
+        final checkingIN = await deshiFarmerAPI.checkInAPI(
+          event.token,
+          null,
+          event.imageFile,
+          getCurrentPosition.latitude,
+          getCurrentPosition.longitude,
+        );
 
-    //   emit(const AttendanceInitial());
-    // }
+        // print('check in from BLOCK -> ${checkingIN.$1} : ${checkingIN.$2}');
+        if (checkingIN.$1) {
+          final todaysAtten =
+              await deshiFarmerAPI.getTodaysAttendance(event.token);
+          print('todays attendance From BLOC ${todaysAtten}');
+          if (todaysAtten != null) {
+            emit(AttendanceSuccess(todays: todaysAtten));
+          }
+        }
+      } catch (e) {
+        print('error happend on getting location $e');
+        emit(AttendanceError(msg: e.toString()));
+      }
+      // print('')
+
+      emit(const AttendanceInitial());
+    }
   }
 
   FutureOr<void> _onCheckInFromFuture(CheckInFromFuture event, emit) {
