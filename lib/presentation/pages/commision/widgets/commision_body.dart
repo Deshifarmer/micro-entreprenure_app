6b44946642bdeacc31d4aa52fce9341101e4caec
RomjanHossain <romjan@skiff.com>
2023-09-24 13:57:16 +0600

    commission homne page

--- lib/presentation/pages/commision/widgets/commision_body.dart
+++ lib/presentation/pages/commision/widgets/commision_body.dart
@@ -1,4 +1,7 @@
 import 'package:deshifarmer/presentation/pages/commision/bloc/bloc.dart';
+import 'package:deshifarmer/presentation/pages/commision/components/commision_card.dart';
+import 'package:deshifarmer/presentation/pages/commision/widgets/all_commission.dart';
+import 'package:deshifarmer/presentation/widgets/size_config.dart';
 import 'package:flutter/material.dart';
 
 /// {@template commision_body}
@@ -14,7 +17,41 @@ class CommisionBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<CommisionBloc, CommisionState>(
       builder: (context, state) {
-        return const Center(child: Text('commision page'));
+        return Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: [
+              Text(
+                'আপনার সেলস এর কমিশন এর তালিকা',
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      fontWeight: FontWeight.w400,
+                    ),
+              ),
+              SizedBox(height: getProportionateScreenHeight(10)),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: CommisionCard(
+                      isFilled: true,
+                      subtitle: '৳ 0',
+                      title: 'এই মাস',
+                    ),
+                  ),
+                  Expanded(
+                    child: CommisionCard(
+                      isFilled: false,
+                      subtitle: '৳ 0',
+                      title: 'সর্বমোট',
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: getProportionateScreenHeight(10)),
+              const AllCommissionBox(),
+            ],
+          ),
+        );
       },
     );
   }
