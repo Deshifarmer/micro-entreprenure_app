875393f7848231e291a933106adf696a33fc83d2
RomjanHossain <romjanhossain726526@gmail.com>
2023-09-24 13:53:59 +0600

    unnecessary files and imports removed

--- lib/presentation/cubit/groups/get_group_cubit.dart
+++ lib/presentation/cubit/groups/get_group_cubit.dart
@@ -2,9 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:deshifarmer/core/error/exceptions.dart';
 import 'package:deshifarmer/data/repositories/farmer_group_repo_impl.dart';
 import 'package:deshifarmer/domain/entities/group_field_entity/all_farmer_group_field.dart';
-import 'package:equatable/equatable.dart';
-
-part 'get_group_state.dart';
 
 //getGroupFields
 class GetGroupCubit extends Cubit<AllFarmerGroupFieldResp> {
