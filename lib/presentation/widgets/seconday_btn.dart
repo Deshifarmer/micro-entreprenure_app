c61b14c3b4e44c1bc4fe857cd612babf95ae1d11
RomjanHossain <romjan@skiff.com>
2023-09-24 09:37:56 +0600

    another secondary button

--- lib/presentation/widgets/seconday_btn.dart
+++ lib/presentation/widgets/seconday_btn.dart
@@ -35,3 +35,40 @@ class SecondayButtonGreen extends StatelessWidget {
     );
   }
 }
+
+class ErrorButtonRed extends StatelessWidget {
+  const ErrorButtonRed({
+    required this.onpress,
+    required this.title,
+    this.btnColor,
+    super.key,
+  });
+
+  final String title;
+  final void Function()? onpress;
+  final Color? btnColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStatePropertyAll(
+            const Color(0xffFF4D4D),
+          ),
+          shape: MaterialStatePropertyAll(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        onPressed: onpress,
+        child: Text(
+          title,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
