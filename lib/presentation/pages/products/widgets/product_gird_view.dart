b35aee23de9723cb65552c9d816b2e6887116ae4
RomjanHossain <romjan@skiff.com>
2023-09-26 18:50:17 +0600

    product grid view fixed

--- lib/presentation/pages/products/widgets/product_gird_view.dart
+++ lib/presentation/pages/products/widgets/product_gird_view.dart
@@ -2,6 +2,7 @@ import 'package:deshifarmer/presentation/blocs/products/products_bloc.dart';
 import 'package:deshifarmer/presentation/pages/commision/bloc/bloc.dart';
 import 'package:deshifarmer/presentation/pages/products/bloc/products_bloc.dart';
 import 'package:deshifarmer/presentation/pages/products/components/product_card.dart';
+import 'package:deshifarmer/presentation/widgets/primary_loading_progress.dart';
 import 'package:flutter/material.dart';
 
 class ProductGridViewWidgets extends StatelessWidget {
@@ -15,30 +16,49 @@ class ProductGridViewWidgets extends StatelessWidget {
       builder: (context, state) {
         if (state is ProductSSuccess) {
           // final companyState = context.read<ProductsBloc>().state;
-          return BlocConsumer<ProductsBloc, ProductsState>(
-            listener: (context, companyState) {},
-            builder: (context, companyState) {
-              final allProducts = companyState is ProductComanySelect
-                  ? state.productDatas
-                      .where(
-                        (element) =>
-                            element.company_id == companyState.companyID,
-                      )
-                      .toList()
-                  : state.productDatas;
-              return GridView.builder(
-                shrinkWrap: true,
-                itemCount: allProducts.length,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisExtent: 320,
-                ),
-                itemBuilder: (context, index) {
-                  final product = allProducts.elementAt(index);
-                  return ProductCard(product: product);
-                },
-              );
+          return BlocConsumer<ProductsBBloc, ProductsSState>(
+            listener: (context, productState) {},
+            builder: (context, productState) {
+              // final allProducts = companyState is ProductComanySelect
+              //     ? state.productDatas
+              //         .where(
+              //           (element) =>
+              //               element.company_id == companyState.companyID,
+              //         )
+              //         .toList()
+              //     : state.productDatas;
+              if (productState is ProductSSuccess) {
+                return GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: productState.productDatas.length,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisExtent: 320,
+                  ),
+                  itemBuilder: (context, index) {
+                    final product = productState.productDatas.elementAt(index);
+                    return ProductCard(product: product);
+                  },
+                );
+              } else {
+                return const Center(
+                  child: Text('Failed'),
+                );
+              }
+              // return GridView.builder(
+              //   shrinkWrap: true,
+              //   itemCount: allProducts.length,
+              //   physics: const NeverScrollableScrollPhysics(),
+              //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              //     crossAxisCount: 2,
+              //     mainAxisExtent: 320,
+              //   ),
+              //   itemBuilder: (context, index) {
+              //     final product = allProducts.elementAt(index);
+              //     return ProductCard(product: product);
+              //   },
+              // );
             },
           );
         }
@@ -50,10 +70,8 @@ class ProductGridViewWidgets extends StatelessWidget {
           );
         }
 
-        return Center(
-          child: CircularProgressIndicator(
-            color: Colors.green[600],
-          ),
+        return const Center(
+          child: PrimaryLoadingIndicator(),
         );
       },
     );
