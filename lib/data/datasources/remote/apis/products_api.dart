6ef55e7eccedb4525c74f16c8180cf832143667e
RomjanHossain <romjan@skiff.com>
2023-10-01 17:36:16 +0600

    product as an indivisual

---
+++ lib/data/datasources/remote/apis/products_api.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'dart:isolate';
+
+import 'package:deshifarmer/domain/entities/products_entity/product_entity.dart';
+import 'package:deshifarmer/presentation/utils/get_product_url_utils.dart';
+import 'package:http/http.dart' as http;
+
+class ProductFetchAPI {
+  final Map<String, String> _headers = <String, String>{
+    'Accept': 'application/json',
+    // 'Authorization': 'Bearer ${ApiKeys.urlBayToken}',
+  };
+
+  // get producttt from search
+  Future<ProductEntity?> getProductSearch(
+    String token,
+    String? company,
+    String? category,
+    String? query,
+  ) async {
+    final auth = <String, String>{
+      'Authorization': 'Bearer $token',
+    };
+    final url = getTheProductURL(
+      query,
+      company,
+      category,
+    );
+    print('search url -> $url');
+    try {
+      _headers.addAll(auth);
+      final response = await http.get(
+        url,
+        headers: _headers,
+      );
+      print('search response -> ${response.statusCode}');
+      if (response.statusCode == 200) {
+        final result = await Isolate.run(() => json.decode(response.body))
+            as Map<String, dynamic>;
+        try {
+          final successResonse = ProductEntity.fromJson(result);
+          // return Success<ProductEntity, Exception>(successResonse);
+          return successResonse;
+        } catch (e) {
+          print('error -> $e');
+          result.forEach((key, value) {});
+        }
+        // return ServerFailor<ProductEntity, Exception>(
+        //   Exception('Server failor'),
+        // );
+        return null;
+      } else {
+        // return ServerFailor<ProductEntity, Exception>(
+        //   Exception('Server failor'),
+        // );
+        return null;
+      }
+    } catch (e) {
+      // return ServerFailor<ProductEntity, Exception>(
+      //   Exception('Server failor -> $e'),
+      // );
+      return null;
+    }
+  }
+}
