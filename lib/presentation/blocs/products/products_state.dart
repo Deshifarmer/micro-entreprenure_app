a6a464af1a07ba0c2ab344172886da2e9576c577
RomjanHossain <romjan@skiff.com>
2023-10-01 17:36:41 +0600

    products bloc and impl commented

--- lib/presentation/blocs/products/products_state.dart
+++ lib/presentation/blocs/products/products_state.dart
@@ -1,23 +1,29 @@
-part of 'products_bloc.dart';
+// part of 'products_bloc.dart';
 
-abstract class ProductsSState extends Equatable {
-  const ProductsSState();
+// abstract class ProductsSState extends Equatable {
+//   const ProductsSState();
 
-  @override
-  List<Object> get props => [];
-}
+//   @override
+//   List<Object> get props => [];
+// }
 
-class ProductsIInitial extends ProductsSState {}
+// class ProductsIInitial extends ProductsSState {}
 
-class ProductFFailed extends ProductsSState {}
+// class ProductFFailed extends ProductsSState {}
 
-class ProductSSuccess extends ProductsSState {
-  const ProductSSuccess({
-    required this.productEntity,
-    required this.productDatas,
-  });
-  final ProductEntity productEntity;
-  final List<ProductData> productDatas;
-  @override
-  List<Object> get props => [productEntity];
-}
+// class ProductSSuccess extends ProductsSState {
+//   const ProductSSuccess({
+//     required this.productEntity,
+//     required this.productDatas,
+//   });
+//   final ProductEntity productEntity;
+//   final List<ProductData> productDatas;
+
+//   @override
+//   List<Object> get props => [
+//         productEntity,
+//         productDatas,
+//       ];
+// }
+
+// class ProductSLoading extends ProductsSState {}
