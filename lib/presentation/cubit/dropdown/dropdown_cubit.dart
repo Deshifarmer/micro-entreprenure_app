2b64448a245c67bdb62c1d42cdd97593e3db1cdb
RomjanHossain <romjan@skiff.com>
2023-10-02 15:13:13 +0600

    dropdown for farmer

--- lib/presentation/cubit/dropdown/dropdown_cubit.dart
+++ lib/presentation/cubit/dropdown/dropdown_cubit.dart
@@ -1,8 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:deshifarmer/core/params/payment_params.dart';
+import 'package:deshifarmer/domain/entities/farmer_entity/farmer_entity.dart';
 
-class DropdownCubit extends Cubit<String> {
-  DropdownCubit() : super(PaymentParams.paymentMethods.first);
+class DropdownForPaymentCubit extends Cubit<String> {
+  DropdownForPaymentCubit() : super(PaymentParams.paymentMethods.first);
 
   void changeDropdownValue(String val) => emit(val);
+}
+
+class DropdownForFarmerCubit extends Cubit<FarmerEntity?> {
+  DropdownForFarmerCubit() : super(null);
+
+  void changeDropdownValue(FarmerEntity val) => emit(val);
 }
