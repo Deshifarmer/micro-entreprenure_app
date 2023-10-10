217e27b9cc7d663d1222beee7b8a6b59a291bd8c
RomjanHossain <romjanhossain726526@gmail.com>
2023-10-10 09:47:56 +0600

    removed unused btn

--- lib/presentation/pages/harvest/view/harvest_page.dart
+++ lib/presentation/pages/harvest/view/harvest_page.dart
@@ -1,12 +1,6 @@
-import 'dart:isolate';
 
-import 'package:animations/animations.dart';
 import 'package:deshifarmer/data/datasources/remote/apis/api_source.dart';
-import 'package:deshifarmer/domain/entities/crop_entity/single_crop_entity.dart';
-import 'package:deshifarmer/domain/entities/farmer_entity/all_farmer_entity.dart';
-import 'package:deshifarmer/presentation/blocs/my_farmer/my_farmer_bloc.dart';
 import 'package:deshifarmer/presentation/pages/activity/activity.dart';
-import 'package:deshifarmer/presentation/pages/harvest/pages/harvest_recod_page.dart';
 import 'package:deshifarmer/presentation/pages/harvest/pages/harvest_record_page2.dart';
 import 'package:deshifarmer/presentation/pages/harvest/widgets/harvest_body.dart';
 import 'package:deshifarmer/presentation/pages/login/bloc/login_bloc.dart';
@@ -90,61 +84,6 @@ class _HarvestPageState extends State<HarvestPage> {
               ),
       ),
     );
-  }
-}
-
-class ShowLoadingOnButtonHarvest extends StatefulWidget {
-  const ShowLoadingOnButtonHarvest({
-    super.key,
-  });
-
-  @override
-  State<ShowLoadingOnButtonHarvest> createState() =>
-      _ShowLoadingOnButtonHarvestState();
-}
-
-class _ShowLoadingOnButtonHarvestState
-    extends State<ShowLoadingOnButtonHarvest> {
-  bool isLoading = false;
-  @override
-  Widget build(BuildContext context) {
-    return isLoading
-        ? SecondayButtonGreen(
-            onpress: () async {
-              if (isLoading) {
-                return;
-              }
-              setState(() {
-                isLoading = true;
-              });
-              final loginState = context.read<LoginBloc>().state;
-              final token = loginState is LoginSuccess
-                  ? loginState.successLoginEntity.token
-                  : '';
-              final allFarmerListResp =
-                  await DeshiFarmerAPI().getFarmers2(token);
-
-              final cropList = await DeshiFarmerAPI().getCropFromAnotherAPI();
-
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) => HarvestRecordPage2(
-                    crops: cropList,
-                    allFarmerListResp: allFarmerListResp,
-                  ),
-                ),
-              );
-
-              setState(() {
-                isLoading = false;
-              });
-            },
-            title: 'সেলস রেকর্ড করুন ',
-          )
-        : const Center(
-            child: PrimaryLoadingIndicator(),
-          );
   }
 }
 
