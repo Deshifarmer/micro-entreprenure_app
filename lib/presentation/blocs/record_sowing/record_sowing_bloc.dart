2da9778642fd2e267f6b6a070f11b610eb11cce7
RomjanHossain <romjanhossain726526@gmail.com>
2023-10-11 16:43:16 +0600

    bloc func uncommented

--- lib/presentation/blocs/record_sowing/record_sowing_bloc.dart
+++ lib/presentation/blocs/record_sowing/record_sowing_bloc.dart
@@ -1,5 +1,5 @@
 import 'package:bloc/bloc.dart';
-import 'package:deshifarmer/data/datasources/remote/apis/api_source.dart';
+import 'package:deshifarmer/data/datasources/remote/apis/harvest_api.dart';
 import 'package:deshifarmer/data/models/record_activity_model.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
@@ -11,25 +11,30 @@ part 'record_sowing_state.dart';
 class RecordSowingBloc extends Bloc<RecordSowingEvent, RecordSowingState> {
   RecordSowingBloc() : super(RecordSowingInitial()) {
     on<RecordSowingEvent>((event, emit) {});
-    on<RecordSowingPostEvent>((event, emit) async {
+    on<RecordSowingLoadingStopEvent>((event, emit) {
+      emit(RecordSowingInitial());
+    });
+    on<RecordSowingPostEvent>((RecordSowingPostEvent event, emit) async {
       emit(RecordSowingLoading());
       // final getCurrentPosition = await determinePosition();
-      // print(
-      //     'lat -> ${getCurrentPosition.latitude} ${getCurrentPosition.longitude}');
-      ///! TODO: uncomment this
-      // final result = await deshiFarmerAPI.recordActivity(
-      //     // getCurrentPosition.latitude.toString(),
-      //     event.ram
-      //     // getCurrentPosition.longitude.toString(),
-      //     );
-      // if (result != null) {
-      //   emit(RecordSowingSuccess());
-      //   emit(RecordSowingInitial());
-      // } else {
-      //   emit(RecordSowingFailed());
-      //   emit(RecordSowingInitial());
-      // }
+      final result = await deshiFarmerAPI.recordActivity(
+          // getCurrentPosition.latitude.toString(),
+          event.ram,
+          // getCurrentPosition.longitude.toString(),
+          );
+      if (result != null) {
+        if (result) {
+          emit(RecordSowingSuccess());
+          emit(RecordSowingInitial());
+        } else {
+          emit(RecordSowingFailed());
+          emit(RecordSowingInitial());
+        }
+      } else {
+        emit(RecordSowingFailed());
+        emit(RecordSowingInitial());
+      }
     });
   }
-  DeshiFarmerAPI deshiFarmerAPI = DeshiFarmerAPI();
+  HarvestAPI deshiFarmerAPI = HarvestAPI();
 }
