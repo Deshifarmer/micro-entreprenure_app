12fb9b8067db483ad54de03a38c60d70aa6eaf54
RomjanHossain <romjanhossain726526@gmail.com>
2023-10-16 11:21:31 +0600

    logistic modelllllll

---
+++ lib/presentation/pages/logistic/model/logistic_model.dart
@@ -0,0 +1,12 @@
+class LogisticModel {
+  LogisticModel({
+    required this.from,
+    required this.to,
+    required this.trackType,
+    required this.weight,
+  });
+  final String to;
+  final String from;
+  final String trackType;
+  final String weight;
+}
