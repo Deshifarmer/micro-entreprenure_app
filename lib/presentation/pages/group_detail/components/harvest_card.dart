351dab4c5b2c356b5a8035c88d2d7b351daea2c4
RomjanHossain <romjan@skiff.com>
2023-10-21 10:08:45 +0600

    responsive card harvest

--- lib/presentation/pages/group_detail/components/harvest_card.dart
+++ lib/presentation/pages/group_detail/components/harvest_card.dart
@@ -1,8 +1,8 @@
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:deshifarmer/core/app_strings.dart';
 import 'package:deshifarmer/presentation/utils/deshi_colors.dart';
 import 'package:deshifarmer/presentation/widgets/constraints.dart';
+import 'package:deshifarmer/presentation/widgets/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -25,105 +25,127 @@ class HarvestCard extends StatelessWidget {
   final String qt;
   final String unit;
   final String date;
-  final double? soldPrice;
+  final String? soldPrice;
 
   @override
   Widget build(BuildContext context) {
     // format the date time like (17 Oct, 2023) using intl package
     final formattedDate =
         DateFormat('dd MMM, yyyy').format(DateTime.parse(date));
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    debugPrint('${Strings.getServerOrLocal(ServerOrLocal.server)}$image');
+    return Stack(
       children: [
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            //* THE IMAGE
-            Container(
-              height: 90,
-              width: 90,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: CachedNetworkImage(
-                  imageUrl:
-                      '${Strings.getServerOrLocal(ServerOrLocal.server)}$image',
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
-                  fit: BoxFit.cover,
+            Row(
+              children: [
+                //* THE IMAGE
+                Container(
+                  height: getProportionateScreenHeight(90),
+                  width: getProportionateScreenWidth(90),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: CachedNetworkImage(
+                      imageUrl:
+                          '${Strings.getServerOrLocal(ServerOrLocal.server)}$image',
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            //* Name, farmer name + farmer id, date
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    name,
-                    style: const TextStyle(
-                      fontSize: titleFontLarge,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    '$farmerName #$farmerID',
-                    style: const TextStyle(
-                      fontSize: smallFont,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8),
-                    child: Text(
-                      formattedDate,
-                      style: const TextStyle(
-                        fontSize: smallFont,
-                        fontWeight: FontWeight.w600,
+                //* Name, farmer name + farmer id, date
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        style: const TextStyle(
+                          fontSize: titleFontLarge,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-        //* Quantity + unit and sold
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Text(
-                '$qt $unit',
-                style: const TextStyle(
-                  fontSize: titleFont,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            if (soldPrice != null)
-              Chip(
-                backgroundColor: priceBoxColor,
-                avatar: const Icon(
-                  Icons.playlist_add_check_outlined,
-                  color: Colors.white,
-                ),
-                // labelPadding: EdgeInsets.all(0),
-                label: Text(
-                  'বিক্রিত',
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                        color: Colors.white,
+                      Text(
+                        '$farmerName #$farmerID',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
                       ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          formattedDate,
+                          style: const TextStyle(
+                            fontSize: smallFont,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
+            ),
+            //* Quantity + unit and sold
+            // Column(
+            //   mainAxisAlignment: MainAxisAlignment.end,
+            //   crossAxisAlignment: CrossAxisAlignment.end,
+            //   children: [
+            //     Padding(
+            //       padding: const EdgeInsets.only(bottom: 10),
+            //       child: Text(
+            //         '$qt $unit',
+            //         style: const TextStyle(
+            //           fontSize: titleFont,
+            //           fontWeight: FontWeight.w600,
+            //         ),
+            //       ),
+            //     ),
+            //   ],
+            // ),
           ],
+          // sold
         ),
+        Positioned(
+          top: 10,
+          right: 0,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Text(
+              '$qt $unit',
+              style: const TextStyle(
+                fontSize: titleFont,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+        if (soldPrice != null)
+          Positioned(
+            bottom: -10,
+            right: 0,
+            child: Chip(
+              backgroundColor: priceBoxColor,
+              avatar: const Icon(
+                Icons.playlist_add_check_outlined,
+                color: Colors.white,
+              ),
+              // labelPadding: EdgeInsets.all(0),
+              label: Text(
+                'বিক্রিত',
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+            ),
+          ),
       ],
     );
   }
