c4dc9ba9bc282fc28c97b242edc4f2b4653ac920
RomjanHossain <romjanhossain726526@gmail.com>
2023-10-22 16:15:14 +0600

    settings boolean values

--- lib/data/datasources/local/shared_prefs/local_database_sf.dart
+++ lib/data/datasources/local/shared_prefs/local_database_sf.dart
@@ -23,4 +23,89 @@ class SharedPrefDBServices {
     // get value
     return prefs.setString(_loginKey, token);
   }
+
+  /*
+    Some boolan values that used in the settings page
+  */
+
+  /// is notification enabled
+  //* get NOTIFICATION ENABLED
+  Future<bool?> isNotificationEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    // get value
+    return prefs.getBool('isNotificationEnabled');
+  }
+
+  //* set NOTIFICATION ENABLED
+  Future<bool> setNotificationEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    // get value
+    return prefs.setBool('isNotificationEnabled', true);
+  }
+  //* unset NOTIFICATION ENABLED
+  Future<bool> unsetNotificationEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    // get value
+    return prefs.setBool('isNotificationEnabled', false);
+  }
+
+  /// is email enabled
+  //* get EMAIL ENABLED
+  Future<bool?> isEmailEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    // get value
+    return prefs.getBool('isEmailEnabled');
+  }
+
+  //* set EMAIL ENABLED
+  Future<bool> setEmailEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    // get value
+    return prefs.setBool('isEmailEnabled', true);
+  }
+  //* unset EMAIL ENABLED
+  Future<bool> unsetEmailEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    // get value
+    return prefs.setBool('isEmailEnabled', false);
+  }
+
+  //* get WEAHTER ENABLED
+  Future<bool?> isWeatherEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    // get value
+    return prefs.getBool('isWeatherEnabled');
+  }
+  
+
+  //* set WEAHTER ENABLED
+  Future<bool> setWeatherEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.setBool('isWeatherEnabled', true);
+  }
+  //* unset WEAHTER ENABLED
+  Future<bool> unsetWeatherEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.setBool('isWeatherEnabled', false);
+  }
+
+  //* get PROMOTIONALS ENABLED
+  Future<bool?> isPromotionalsEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    // get value
+    return prefs.getBool('isPromotionalsEnabled');
+  }
+
+  //* set PROMOTIONALS ENABLED
+  Future<bool> setPromotionalsEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    // get value
+    return prefs.setBool('isPromotionalsEnabled', true);
+  }
+    //* unset PROMOTIONALS ENABLED
+  Future<bool> unsetPromotionalsEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    // get value
+    return prefs.setBool('isPromotionalsEnabled', false);
+  }
 }
