3fa5244c2d165a1732fe4a660b34da0cd65651a0
RomjanHossain <romjan@skiff.com>
2023-10-25 17:58:50 +0600

    chcking network init

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,12 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:deshifarmer/app/app.dart';
 import 'package:deshifarmer/bootstrap.dart';
+import 'package:deshifarmer/services/connection_services.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  bootstrap(() => const App());
+late final ConnectivityService connectionService;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  connectionService = await ConnectivityService(Connectivity()).init();
+  await bootstrap(() => const App());
 }
