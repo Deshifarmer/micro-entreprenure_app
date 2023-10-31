e2caa5ae06a338e59d9a4e7b845b5beccd005b6d
RomjanHossain <romjanhossain726526@gmail.com>
2023-10-31 13:17:16 +0600

    order repo impls the new order api

--- lib/data/repositories/order_repo_impl.dart
+++ lib/data/repositories/order_repo_impl.dart
@@ -1,10 +1,10 @@
 import 'package:deshifarmer/core/error/exceptions.dart';
-import 'package:deshifarmer/data/datasources/remote/apis/api_source.dart';
+import 'package:deshifarmer/data/datasources/remote/apis/order_api.dart';
 import 'package:deshifarmer/domain/entities/orders_entity/all_orders.dart';
 import 'package:deshifarmer/domain/repositories/order_repo.dart';
 
 class UserOrderRepoImpl extends UserOrderRepo {
-  final DeshiFarmerAPI _dfAPi = DeshiFarmerAPI();
+  final OrderAPI _dfAPi = OrderAPI();
 
   @override
   Future<Result<AllOrdersEntity, Exception>> userOrder(
