b6c6072f67e4dee29eec116b732b5c96eddc804e
RomjanHossain <romjanhossain726526@gmail.com>
2023-10-31 14:27:54 +0600

    new order entity paginate

--- lib/domain/entities/orders_entity/all_orders.dart
+++ lib/domain/entities/orders_entity/all_orders.dart
@@ -1,7 +1,23 @@
 import 'package:deshifarmer/domain/entities/orders_entity/order_response_entity.dart';
+import 'package:deshifarmer/domain/entities/products_entity/product_links_entity.dart';
+import 'package:deshifarmer/domain/entities/products_entity/product_meta_entity.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'all_orders.g.dart';
+
+@JsonSerializable()
 class AllOrdersEntity {
-  AllOrdersEntity(this.orders);
+  AllOrdersEntity({
+    required this.data,
+    required this.links,
+    required this.meta,
+  });
 
-  final List<OrderEntity> orders;
+  /// factory.
+  factory AllOrdersEntity.fromJson(Map<String, dynamic> json) =>
+      _$AllOrdersEntityFromJson(json);
+
+  final List<OrderEntity> data;
+  final ProductLinks? links;
+  final ProductMeta? meta;
 }
