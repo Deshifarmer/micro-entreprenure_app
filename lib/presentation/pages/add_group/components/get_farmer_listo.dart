c7b22bd549cc18f52fbc65a053d5c40e1b1a0ca3
RomjanHossain <romjanhossain726526@gmail.com>
2023-11-08 14:54:18 +0600

    show create group if empty group
    show create button if theres no group found

--- lib/presentation/pages/add_group/components/get_farmer_listo.dart
+++ lib/presentation/pages/add_group/components/get_farmer_listo.dart
@@ -1,27 +1,49 @@
+import 'package:deshifarmer/data/datasources/remote/apis/api_source.dart';
 import 'package:deshifarmer/domain/entities/group_field_entity/all_farmer_group_field.dart';
-import 'package:deshifarmer/presentation/pages/add_farmer/add_farmer.dart';
-import 'package:deshifarmer/presentation/pages/add_group/add_group.dart';
 import 'package:deshifarmer/presentation/pages/add_group/components/group_list_with_search.dart';
-import 'package:deshifarmer/services/cubit/groups/get_group_cubit.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:deshifarmer/presentation/widgets/primary_loading_progress.dart';
 import 'package:flutter/material.dart';
 
-class GetFarmerListo extends StatelessWidget {
-  const GetFarmerListo({super.key});
+class GetFarmerListo extends StatefulWidget {
+  const GetFarmerListo({super.key, required this.token});
+  final String token;
 
   @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<GetGroupCubit, AllFarmerGroupFieldResp>(
-      listener: (context, state) {},
-      builder: (context, state) {
-        if (state.farmers.isEmpty) {
-          return const SizedBox.shrink();
-        }
+  State<GetFarmerListo> createState() => _GetFarmerListoState();
+}
 
-        return GroupListsGrid(
-          farmers: state.farmers,
-        );
+class _GetFarmerListoState extends State<GetFarmerListo> {
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () {
+        return Future(() {
+          setState(() {});
+        });
       },
+      child: FutureBuilder<AllFarmerGroupFieldResp?>(
+        future: DeshiFarmerAPI().getGroupFields2(widget.token),
+        builder: (BuildContext context,
+            AsyncSnapshot<AllFarmerGroupFieldResp?> snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data!.farmers.isEmpty) {
+              debugPrint('Emptry group now showing nothing');
+
+              return const GroupListsGrid(
+                farmers: [],
+              );
+            }
+
+            return GroupListsGrid(
+              farmers: snapshot.data!.farmers,
+            );
+          } else {
+            return const Center(
+              child: PrimaryLoadingIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 }
