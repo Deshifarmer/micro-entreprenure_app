0ff66dd01fe86e2ab55a14767831031296f05ae8
RomjanHossain <romjanhossain726526@gmail.com>
2023-12-13 17:51:32 +0600

    show native toasted on success

---
+++ lib/presentation/utils/others_helper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void showToast(String msg, Color? color) {
+  Fluttertoast.cancel();
+  Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_LONG,
+    gravity: ToastGravity.BOTTOM,
+    backgroundColor: color,
+    textColor: Colors.white,
+    fontSize: 16,
+  );
+}
